e1e9123b2478c0525c9b8dce0012eda28fb99be5
maciejtreder <contact@maciejtreder.com>
2017-12-06 08:40:27 -0500

    Saving

--- src/externalComponent/external.component.ts
+++ src/externalComponent/external.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'external-component',
-    template: `<div class="content"><h3>I am external!</h3></div>`
+    templateUrl: `./external.component.html`
 })
 export class ExternalComponent {}
